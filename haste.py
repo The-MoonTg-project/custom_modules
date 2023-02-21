d9371b3a39e7dd7e259658324a381ea8a062a370
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2023-02-21 23:11:55 +0530

    update haste.py

--- haste.py
+++ haste.py
@@ -9,19 +9,19 @@ BASE = "https://hastebin.com"
 
 @Client.on_message(filters.command("haste", prefix) & filters.me)
 @with_reply
-async def haste(client: Client, message: Message):
+def haste(client: Client, message: Message):
     reply = message.reply_to_message
 
     if reply.text is None:
         return
 
-    await message.delete()
+    message.delete()
 
     result = requests.post(
         "{}/documents".format(BASE), data=reply.text.encode("UTF-8")
     ).json()
 
-    await message.reply(
+    message.reply(
         "{}/{}.py".format(BASE, result["key"]), reply_to_message_id=reply.message_id
     )
 
