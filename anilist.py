5e0325a6bf2a2dbe8a8de73035e99311e3edcac9
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2023-02-22 13:30:36 +0530

    Update anilist.py

--- anilist.py
+++ anilist.py
@@ -39,7 +39,7 @@ from tracemoepy.errors import ServerError
 from motor.motor_asyncio import AsyncIOMotorClient
 from motor.core import AgnosticDatabase, AgnosticCollection
 
-from utils.db import Database.get_collection
+from utils.db import Database
 
 class Dynamic:
 
