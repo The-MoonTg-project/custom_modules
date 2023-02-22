0727b7efbe3fd33e91dae6290ed2535560754be6
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2023-02-22 13:57:44 +0530

    Update anilist.py

--- anilist.py
+++ anilist.py
@@ -19,7 +19,7 @@ from datetime import datetime
 
 from utils.scripts import import_library
 from utils.misc import modules_help, prefix
-from pyrogram import Client, filters
+from pyrogram import Client, filters, Message
 from utils import config
 from utils.scripts import with_reply
 
