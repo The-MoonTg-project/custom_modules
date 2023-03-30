e7878451b269ac4429bc07795c9ac5108cd80cc2
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2023-03-30 18:14:57 +0530

    Update ytdl.py

--- ytdl.py
+++ ytdl.py
@@ -7,7 +7,7 @@ import os
 from asyncio import get_event_loop
 
 
-youtube_dl = import_library("yt-dlp==2023.3.4")
+youtube_dl = import_library("yt-dlp==2023.2.17")
 pillow = import_library("PIL")
 YoutubeDL = youtube_dl.YoutubeDL
 DownloadError = youtube_dl.utils.DownloadError
