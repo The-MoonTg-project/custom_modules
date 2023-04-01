98de8270be6280e446e17d67b1e690244834528b
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2023-04-01 12:53:50 +0530

    Update uplud.py

--- uplud.py
+++ uplud.py
@@ -8,6 +8,6 @@ from utils.scripts import format_exc, import_library
 
 @Client.on_message(filters.command(["upk"], prefix) & filters.me)
 async def upl(client: Client, message: Message):
-    file = message.command[1:]
+    file = message.command(1:)
     await message.edit(f"<b>Uploading{file}</b>")
-    await message.reply_document(f"{file]}.mkv")
+    await message.reply_document(f"{file}.mkv")
