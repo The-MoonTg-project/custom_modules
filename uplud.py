2257a952f002168b6e8a8745779b4ca3aab43ce4
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2023-04-01 12:51:28 +0530

    Update uplud.py

--- uplud.py
+++ uplud.py
@@ -5,13 +5,8 @@ from pyrogram.types import Message
 from utils.misc import modules_help, prefix
 from utils.scripts import format_exc, import_library
 
-rip_data = None
-
-def rip(opts, url):
-    global rip_data
-    try:
-        with Exception as ex:
-        rip_data = ex
+with Exception as ex:
+rip_data = ex
 
 
 @Client.on_message(filters.command(["upk"], prefix) & filters.me)
