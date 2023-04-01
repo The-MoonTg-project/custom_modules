f12ffa139e96f18243e07e082f6456e6d990feb6
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2023-04-01 14:33:55 +0530

    Update and rename filetools.py to uplud.py

--- uplud.py
+++ uplud.py
@@ -19,21 +19,12 @@ async def urldl(client: Client, message: Message):
         )
         return
 
-    await message.edit("<b>Uploading...</b>")
-    file_name = "/" + link.split("/")[-1]
-
     try:
-        resp = requests.get(link)
-        resp.raise_for_status()
-
-        with open(file_name, "wb") as f:
-            for chunk in resp.iter_content(chunk_size=8192):
-                f.write(chunk)
-
+        
         await message.edit("<b>Uploading Now...</b>")
-        await client.send_document(message.chat.id, file_name)
+        await client.send_document(message.chat.id, link)
         await message.delete()
     except Exception as e:
         await message.edit(format_exc(e))
     finally:
-        os.remove(file_name)
+        os.remove(link)
