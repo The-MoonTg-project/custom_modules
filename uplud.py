af65ae4dc7a5f27a5951dab369d4b1b07ac74dcc
AbhiTheModder <85984486+AbhiTheModder@users.noreply.github.com>
2023-04-01 12:36:54 +0530

    Update uplud.py

--- uplud.py
+++ uplud.py
@@ -7,7 +7,9 @@ from utils.scripts import format_exc, import_library
 
 @Client.on_message(filters.command(["upl"], prefix) & filters.me)
 async def ytdl_handler(client: Client, message: Message):
-try:
-        file = message.command[1:]
+    
+    file = message.command[1:]
+    
     await message.edit("<b>Whom should I gmute?</b>")
-    await message.reply(f"{file}")
+    
+await message.reply(f"{file}")
