b86ec3abc0853fb7bd2c04651d8d60d82c2d8df9
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2023-12-18 23:04:14 +0530

    Add: ai.py
    Gemini Ai

---
+++ ai.py
@@ -0,0 +1,45 @@
+# This scripts contains use cases for userbots
+# This is used on my Moon-Userbot: https://github.com/The-MoonTg-project/Moon-Userbot
+# YOu can check it out for uses example
+import os
+import google.generativeai as genai
+
+from pyrogram import Client, filters, enums
+from pyrogram.types import Message
+
+from utils.misc import modules_help, prefix
+from utils.scripts import format_exc
+from utils.config import gemini_key
+
+
+
+genai.configure(api_key=gemini_key)
+genai.configure(api_key=API_KEY)
+
+model = genai.GenerativeModel('gemini-pro')
+
+
+@Client.on_message(filters.command("gemini", prefix) & filters.me)
+async def say(_, message: Message):
+    try:
+        await message.edit_text("<code>Please Wait...</code>")
+
+        if len(message.command) > 1:
+         prompt = message.text.split(maxsplit=1)[1]
+        elif message.reply_to_message:
+         prompt = message.reply_to_message.text
+        else:
+         prompt = "Hi Gemini"
+         return
+
+        chat = model.start_chat()
+        response = chat.send_message(prompt)
+
+        await message.edit_text(f"**Question:**`{prompt}`\n**Answer:** {response.text}", parse_mode=enums.ParseMode.MARKDOWN)
+    except Exception as e:
+        await message.edit_text(f"An error occurred: {format_exc(e)}")
+
+
+modules_help["gemini"] = {
+    "gemini [prompt]*": "Ask questions with Ai",
+}
