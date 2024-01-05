af6a7f2fe1beb52e45df2110564ecd7e9ed1fc64
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-01-05 16:10:11 +0000

    Add: backup your current session string to saved messages

---
+++ session.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters, enums
+from pyrogram.types import Message
+
+from utils.misc import modules_help, prefix
+
+
+@Client.on_message(filters.command("amogus", prefix) & filters.me)
+async def amogus(client: Client, message: Message):
+    first_name = (client.get_me()).first_name
+    string_session_ = f"<b><u>String Session For {first_name}</b></u> \n<code>{client.export_session_string()}</code>"
+    client.send_message("me", string_session_, parse_mode=enums.ParseMode.HTML, disable_web_page_preview=True)
+    print(f"String Has Been Sent To Your Saved Message : {first_name}")
+
+modules_help["session"] = {
+    "session": "DANGER... Use with Caution... Backup session to saved messages"
+}
