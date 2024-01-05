cf1d94cef989a6200fb902e009cc5c2e9cfc74be
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-01-05 16:35:17 +0000

    demn it

--- session.py
+++ session.py
@@ -8,7 +8,7 @@ from utils.misc import modules_help, prefix
 async def session(client: Client, message: Message):
     b_f = await client.get_me()
     u_n = b_f.username
-    string_session_ = f"<b><u>String Session For {u_n}</b></u> \n<code>{client.export_session_string()}</code>"
+    string_session_ = await f"<b><u>String Session For {u_n}</b></u> \n<code>{client.export_session_string()}</code>"
     await client.send_message("me", string_session_, parse_mode=enums.ParseMode.HTML, disable_web_page_preview=True)
     await message.edit_text(f"String Has Been Sent To Your Saved Message : {u_n}")
 
