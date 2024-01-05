83d79532a09b0519dad58923e969404a99a738f2
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-01-05 16:20:45 +0000

    hehe

--- session.py
+++ session.py
@@ -6,10 +6,11 @@ from utils.misc import modules_help, prefix
 
 @Client.on_message(filters.command("session", prefix) & filters.me)
 async def session(client: Client, message: Message):
-    first_name = (client.get_me()).first_name
-    string_session_ = f"<b><u>String Session For {first_name}</b></u> \n<code>{client.export_session_string()}</code>"
+    b_f = await client.get_me()
+    u_n = b_f.username
+    string_session_ = f"<b><u>String Session For {u_n}</b></u> \n<code>{client.export_session_string()}</code>"
     client.send_message("me", string_session_, parse_mode=enums.ParseMode.HTML, disable_web_page_preview=True)
-    print(f"String Has Been Sent To Your Saved Message : {first_name}")
+    print(f"String Has Been Sent To Your Saved Message : {u_n}")
 
 modules_help["session"] = {
     "session": "DANGER... Use with Caution... Backup session to saved messages"
