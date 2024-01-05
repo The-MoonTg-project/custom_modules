47020764870fc62f2bfdb344e9085414377d185b
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-01-05 16:11:39 +0000

    Fix: me lol

--- session.py
+++ session.py
@@ -4,8 +4,8 @@ from pyrogram.types import Message
 from utils.misc import modules_help, prefix
 
 
-@Client.on_message(filters.command("amogus", prefix) & filters.me)
-async def amogus(client: Client, message: Message):
+@Client.on_message(filters.command("session", prefix) & filters.me)
+async def session(client: Client, message: Message):
     first_name = (client.get_me()).first_name
     string_session_ = f"<b><u>String Session For {first_name}</b></u> \n<code>{client.export_session_string()}</code>"
     client.send_message("me", string_session_, parse_mode=enums.ParseMode.HTML, disable_web_page_preview=True)
