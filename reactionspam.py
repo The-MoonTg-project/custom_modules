3a810dcbbcf9782dd3ea0b0a1763e9a974936db8
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-01-08 02:16:29 +0530

    Update reactionspam.py

--- reactionspam.py
+++ reactionspam.py
@@ -1,4 +1,4 @@
-import asyncio
+import asyncio, random
 from pyrogram import Client, filters, enums
 from pyrogram.raw import functions
 from pyrogram.types import Message
@@ -27,26 +27,14 @@ emojis = [
 
 @Client.on_message(filters.command("reactspam", prefix) & filters.me)
 async def reactspam(client: Client, message: Message):
-    if len(message.command) > 1:
-        amount = int(message.text.split(None, 2)[1])
-        print(amount)
-    else:
-        message.edit_text("amount not given")
-    if len(message.command) > 2:
-        reaction = message.text.split(None, 2)[2]
-        print(reaction)
-    else:
-        message.edit_text("reaction not given")
     await message.edit(f"<b>One moment...</b>", parse_mode=enums.ParseMode.HTML)
-    for i in range(amount):
-        if reaction in emojis:
-            try:
-                await client.send_reaction(message.chat.id, message_id=message.id, emoji=reaction)
-            except Exception as e:
-                return await message.edit(format_exc(e), parse_mode=enums.ParseMode.HTML)
-        else:
-            return await message.edit(f"<b>You can't use that emoji...</b>", parse_mode=enums.ParseMode.HTML)
-    await message.edit(f"<b>Done!</b>", parse_mode=enums.ParseMode.HTML)
+    try:
+        selected_emojis = random.sample(emojis, 3)
+        print(selected_emojis)
+        await client.send_reaction(message.chat.id, message_id=message.reply_to_message.id, emoji=selected_emojis)
+        await message.delete()
+    except Exception as e:
+        return await message.edit_text(format_exc(e))
 
 
 modules_help["reactionspam"] = {"reactspam [amount]* [emoji]*": "spam reactions"}
