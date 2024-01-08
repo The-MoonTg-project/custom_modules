17410e499e1197c66fb3d6a20675f24e084bd417
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-01-08 10:18:00 +0530

    Update mediasaver.py

--- mediasaver.py
+++ mediasaver.py
@@ -18,7 +18,8 @@ async def msave(client: Client, message: Message):
     await message.delete()
 
     path = await message.reply_to_message.download()
-    await getattr(client, "send_" + media)("me", path)
+    # await getattr(client, "send_" + media)("me", path)
+    await client.send_photo("me", path)
     os.remove(path)
 
 
