3e61c9d478969a04eac8c1dc7585c955ed5a648a
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-02-05 11:17:04 +0530

    Add: approveall.py

---
+++ approveall.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from utils.misc import modules_help, prefix
+
+
+@Client.on_message(filters.command("apall", prefix) & filters.me)
+async def example_edit(client: Client, message: Message):
+    chat_id = message.chat.id
+    await client.approve_all_chat_join_requests(chat_id)
+    await message.edit_text("Done")
+    await message.delete()
+
+modules_help["approveall"] = {
+    "apall": "approve all pending join requests",
+}
