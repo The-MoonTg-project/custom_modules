d92bbd984e0e1eccae35853096a713822ccbaecc
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-02-12 10:22:38 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -10,7 +10,7 @@ from utils.misc import modules_help, prefix
 from utils.scripts import format_exc, import_library
 from utils.config import gemini_key
 
-google = import_library("google", "google-generativeai")
+import_library("google-generativeai")
 import google.generativeai as genai
 
 genai.configure(api_key=gemini_key)
