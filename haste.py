e64f52c93735478d5836f39b524e596a4d57529a
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-04-30 07:44:39 +0530

    Update haste.py

--- haste.py
+++ haste.py
@@ -4,8 +4,11 @@ from pyrogram.types import Message
 from utils.misc import modules_help, prefix
 from utils.scripts import with_reply
 
-BASE = "https://www.toptal.com/developers/hastebin"
+BASE = "https://hastebin.com"
 
+headers = {
+  'Authorization': 'Bearer YOURTOKEN'
+}
 
 @Client.on_message(filters.command("haste", prefix) & filters.me)
 @with_reply
@@ -17,13 +20,25 @@ async def haste(client: Client, message: Message):
 
     await message.delete()
 
-    result = requests.post(
-        "{}/documents".format(BASE), data=reply.text.encode("UTF-8")
-    ).json()
+    try:
+        response = requests.post(
+            "{}/documents".format(BASE), data=reply.text.encode("UTF-8"). headers=headers
+        )
+        response.raise_for_status() # Raises an HTTPError if the response was unsuccessful
+        result = response.json()
+    except requests.exceptions.HTTPError as http_err:
+        await message.reply(f"HTTP error occurred: {http_err}")
+        return
+    except requests.exceptions.RequestException as err:
+        await message.reply(f"Error occurred: {err}")
+        return
+    except ValueError:
+        await message.reply("Error: Response is not in JSON format.")
+        return
 
     await message.reply(
-        "{}/{}.py".format(BASE, result["key"]), reply_to_message_id=reply.message_id, parse_mode=enums.ParseMode.HTML
+        "{}/{}.py".format(BASE, result["key"]),
+        reply_to_message_id=reply.message_id,
     )
-
 
 modules_help["haste"] = {"haste": "reply to text will upload text to hastebin ;)"}
