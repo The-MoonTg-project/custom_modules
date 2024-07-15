fa8e311a633a350ced99fc6bb48dadb0bd9b8a84
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-07-15 20:58:16 +0530

    Update aimage.py

--- aimage.py
+++ aimage.py
@@ -14,11 +14,19 @@ from utils.config import gemini_key
 
 genai.configure(api_key=gemini_key)
 
+generation_config_cook = {
+  "temperature": 0.35,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 1024,
+}
+
 model = genai.GenerativeModel("gemini-pro-vision")
+model_cook = genai.GenerativeModel(model_name="gemini-pro-vision", generation_config=generation_config_cook)
 
 
 @Client.on_message(filters.command("getai", prefix) & filters.me)
-async def say(_, message: Message):
+async def getai(_, message: Message):
     try:
         await message.edit_text("<code>Please Wait...</code>")
         base_img = await message.reply_to_message.download()
@@ -35,7 +43,68 @@ async def say(_, message: Message):
     finally:
         os.remove(base_img)
 
+@Client.on_message(filters.command("aicook", prefix) & filters.me)
+async def aicook(_, message: Message):
+    if message.reply_to_message:
+        try:
+            await message.edit_text("<code>Cooking...</code>")
+
+            base_img = await message.reply_to_message.download()
+
+            img = PIL.Image.open(base_img)
+            cook_img = [
+            "Accurately identify the baked good in the image and provide an appropriate and recipe consistent with your analysis. ",
+            img,
+            ]
+
+            response = model_cook.generate_content(cook_img)
+
+            await message.edit_text(
+                f"{response.text}", parse_mode=enums.ParseMode.MARKDOWN
+            )
+            os.remove(base_img)
+            return
+        except Exception as e:
+            await message.edit_text(str(e))
+    return await message.edit_text("Kindly reply to an image!")
+
+@Client.on_message(filters.command("aiseller", prefix) & filters.me)
+async def aiseller(_, message: Message):
+    if message.reply_to_message:
+        try:
+            await message.edit_text("<code>Generating...</code>")
+            if len(message.command) > 1:
+                taud = message.text.split(maxsplit=1)[1]
+            else:
+                return await message.edit_text(
+                    f"<b>Usage: </b><code>{prefix}aiseller [target audience] [reply to product image]</code>"
+                )
+
+            base_img = await message.reply_to_message.download()
+
+            img = PIL.Image.open(base_img)
+            sell_img = [
+            "Given an image of a product and its target audience, write an engaging marketing description",
+            "Product Image: ",
+            img,
+            "Target Audience: ",
+            taud
+            ]
+
+            response = model.generate_content(sell_img)
+
+            await message.edit_text(
+                f"{response.text}", parse_mode=enums.ParseMode.MARKDOWN
+            )
+            os.remove(base_img)
+            return
+        except Exception:
+            await message.edit_text(f"<b>Usage: </b><code>{prefix}aiseller [target audience] [reply to product image]</code>")
+    return await message.edit_text("Kindly reply to an image!")
+
 
 modules_help["aimage"] = {
     "getai [reply to image]*": "Get details of image with Ai",
+    "aicook [reply to image]*": "Generate Cooking instrunctions of the given food image",
+    "aiseller [target audience] [reply to product image]*": "Generate a promotional message for the given image product for the given target audience"
 }
