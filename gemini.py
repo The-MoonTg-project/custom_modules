9d25f58e05b148c6c3cc7919f030b67066bc1f2f
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-07-19 19:39:41 +0530

    Update gemini models (#35)
    * Update gemini.py
    
    * identation
    
    * Update aimage.py

--- gemini.py
+++ gemini.py
@@ -14,7 +14,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=gemini_key)
 
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 
 @Client.on_message(filters.command("gemini", prefix) & filters.me)
@@ -23,19 +23,22 @@ async def say(_, message: Message):
         await message.edit_text("<code>Please Wait...</code>")
 
         if len(message.command) > 1:
-         prompt = message.text.split(maxsplit=1)[1]
+            prompt = message.text.split(maxsplit=1)[1]
         elif message.reply_to_message:
-         prompt = message.reply_to_message.text
+            prompt = message.reply_to_message.text
         else:
-         await message.edit_text(
-            f"<b>Usage: </b><code>{prefix}gemini [prompt/reply to message]</code>"
-        )
-         return
+            await message.edit_text(
+                f"<b>Usage: </b><code>{prefix}gemini [prompt/reply to message]</code>"
+            )
+            return
 
         chat = model.start_chat()
         response = chat.send_message(prompt)
 
-        await message.edit_text(f"**Question:**`{prompt}`\n**Answer:** {response.text}", parse_mode=enums.ParseMode.MARKDOWN)
+        await message.edit_text(
+            f"**Question:**`{prompt}`\n**Answer:** {response.text}",
+            parse_mode=enums.ParseMode.MARKDOWN,
+        )
     except Exception as e:
         await message.edit_text(f"An error occurred: {format_exc(e)}")
 
