5beb93a60a196265fbd2cde049204a6249ddb109
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-07-24 22:04:09 +0530

    playlist support spotdl

--- spotdl.py
+++ spotdl.py
@@ -1,18 +1,33 @@
 import asyncio
 import os
 import shutil
-from subprocess import STDOUT, check_call, CalledProcessError
+import time
+import requests
+from pytube import YouTube
+
+from subprocess import check_call, CalledProcessError
 from urllib.parse import parse_qs, urlparse
 
-from click import command
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message
 
 from utils.misc import modules_help, prefix
-from utils.scripts import format_module_help, import_library, format_exc
-import requests
+from utils.scripts import format_module_help, import_library, format_exc, progress
 
 spotdl = import_library("spotdl")
+
+exiftool = import_library("exiftool", "pyexiftool")
+from exiftool import ExifToolHelper
+
+
+def get_thumb(file_name):
+    with ExifToolHelper() as et:
+        yt_link = et.get_metadata(file_name)[0]["ID3:Comment-xxx"]
+        yt = YouTube(yt_link)
+        thumb = yt.thumbnail_url
+        with open(f"{file_name}.jpg", "wb") as f:
+            f.write(requests.get(thumb).content)
+    return f"{file_name}.jpg"
 
 
 def get_song_name(url):
@@ -38,7 +53,7 @@ async def spotdl(_, message: Message):
             else message.reply_to_message.text.split("\n")[0]
         )
 
-        await message.edit("<b>Downloading...</b>", parse_mode=enums.ParseMode.HTML)
+        m = await message.edit("<b>Downloading...</b>", parse_mode=enums.ParseMode.HTML)
 
         if not shutil.which("ffmpeg") or message.command[0] == "sdl":
             url = "https://spotify-mp3-downloader.vercel.app/get_download_link?search="
@@ -48,46 +63,55 @@ async def spotdl(_, message: Message):
                 download_link = response.json()["download_link"]
                 song_name = get_song_name(download_link)
                 await message.reply_audio(download_link, caption=f"<b>{song_name}</b>")
+                await asyncio.sleep(0.5)
+                await m.delete()
                 return
             else:
-                await message.edit("api request was unsuccessful.")
+                await m.edit("api request was unsuccessful.")
                 return
 
         try:
             check_call(
-                ["spotdl", "--output", "downloads/", f"{spoti_query}"],
+                ["spotdl", "--output", "spotdl/", f"{spoti_query}"],
                 stdout=open("spotdl_logs.txt", "wb"),
             )
-            logs = open("spotdl_logs.txt", "r", encoding="utf-8").read()
+            logs = "".join(
+                open("spotdl_logs.txt", "r", encoding="utf-8").readlines()[-3:]
+            )
         except CalledProcessError:
             logs = "".join(
                 open("spotdl_logs.txt", "r", encoding="utf-8").readlines()[-3:]
             )
-
-        if "Skipping" in logs:
-            name = logs.split("Skipping ")[1].split(" (file already exists)")[0]
-        elif "Downloaded" in logs:
-            name = logs.split('Downloaded "')[1].split('"')[0]
+        ct = time.time()
+        if "Skipping" or "Downloaded" in logs:
+            for filename in os.listdir("spotdl/"):
+                if filename.endswith(".mp3"):
+                    thumb = get_thumb("spotdl/" + filename)
+                    await message.reply_audio(
+                        audio=f"spotdl/{filename}",
+                        caption=f"<b>{os.path.splitext(filename)[0]}</b>",
+                        thumb=thumb,
+                        progress=progress,
+                        progress_args=(m, ct, "<b>Uploading...</b>"),
+                        reply_to_message_id=(
+                            message.reply_to_message.id
+                            if message.reply_to_message
+                            else None
+                        ),
+                    )
+                    if os.path.exists(thumb):
+                        os.remove(thumb)
+                    if os.path.exists("spotdl/" + filename):
+                        os.remove("spotdl/" + filename)
         else:
-            await message.edit(
-                f"<b>Spotify-Download error:</b>\n<code>{logs}</code>",
-            )
-            return
-        try:
-            await message.reply_audio(
-                f"downloads/{name}.mp3",
-                caption=f"<b>{name}</b>",
-            )
-        except (IndexError, ValueError):
-            return await message.edit("Error: File not found")
-
-        os.remove(f"downloads/{name}.mp3")
-
+            await m.edit(logs)
+        if os.path.exists("spotdl_logs.txt"):
+            os.remove("spotdl_logs.txt")
         await asyncio.sleep(0.5)
-        await message.delete()
+        await m.delete()
 
     except Exception as e:
-        await message.edit(f"<b>Spotify-Download error:</b>\n{format_exc(e)}")
+        await m.edit(f"<b>Spotify-Download error:</b>\n{format_exc(e)}")
 
 
 modules_help["spotdl"] = {
