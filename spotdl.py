21f8700c50c152f692f64c3bb04df0356e08490d
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-07-24 17:26:25 +0530

    spodl fixed

--- spotdl.py
+++ spotdl.py
@@ -1,5 +1,6 @@
 import asyncio
 
+import ffmpeg
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message
 import re
@@ -8,20 +9,16 @@ import re
 from utils.misc import modules_help, prefix
 from utils.scripts import import_library, format_exc
 from subprocess import STDOUT, check_call, CalledProcessError
+import shutil
 import os
 
 
 _spotdl = import_library("spotdl")
 
 
-try:
-    check_call(
-        ["apt-get", "install", "-y", "ffmpeg"],
-        stdout=open(os.devnull, "wb"),
-        stderr=STDOUT,
-    )
+if shutil.which("ffmpeg"):
     ffmpeg = True
-except:
+else:
     ffmpeg = False
 
 
@@ -30,58 +27,68 @@ except:
 async def spotdl_handler(client: Client, message: Message):
     try:
         if len(message.command) == 1 and not message.reply_to_message:
-            await message.edit("<b>Please use:</b> <code>.spotdl [link]</code>",parse_mode=enums.ParseMode.HTML)
+            await message.edit(
+                "<b>Please use:</b> <code>.spotdl [link]</code>",
+                parse_mode=enums.ParseMode.HTML,
+            )
             return
         elif len(message.command) > 1:
-            spoti_query = message.command[1]
+            spoti_query = message.text.split(maxsplit=1)[1]
         elif message.reply_to_message:
-            spoti_query = message.reply_to_message.text.split('\n')[0]
+            spoti_query = message.reply_to_message.text.split("\n")[0]
 
         if not ffmpeg:
             return await message.edit(
                 "<b>Please install (ffmpeg.org) library on your os (and restart Moon-Userbot)</b>",
                 disable_web_page_preview=True,
-                parse_mode=enums.ParseMode.HTML
+                parse_mode=enums.ParseMode.HTML,
             )
 
         await message.edit("<b>Downloading...</b>", parse_mode=enums.ParseMode.HTML)
 
         try:
-            download = check_call(
-                ["spotdl", "--output", "downloads/", f'"{spoti_query}"'],
+            check_call(
+                ["spotdl", "--output", "downloads/", f"{spoti_query}"],
                 stdout=open("spotdl_logs.txt", "wb"),
             )
             logs = open("spotdl_logs.txt", "r", encoding="utf-8").read()
         except CalledProcessError:
-            logs = "".join(open("spotdl_logs.txt", "r", encoding="utf-8").readlines()[-3:])
-            if " as it's already downloaded" in logs:
-                name = logs.split("\" as it's already downloaded")[0].split('"')[-1]
+            logs = "".join(
+                open("spotdl_logs.txt", "r", encoding="utf-8").readlines()[-3:]
+            )
+            if "Skipping" in logs:
+                name = logs.split("Skipping ")[1].split(" (file already exists)")[0]
+            elif "Downloaded" in logs:
+                name = logs.split('Downloaded "')[1].split('"')[0]
+
                 await message.reply_audio(
                     f"downloads/{name}.mp3",
                     caption=f"<b>{name}</b>\n" f"<code>{spoti_query}</code>",
-                    parse_mode=enums.ParseMode.HTML
                 )
                 os.remove(f"downloads/{name}.mp3")
                 return await message.delete()
             else:
                 return await message.edit(
                     f"<b>Spotify-Download error:</b>\n<code>{logs}</code>",
-                    parse_mode=enums.ParseMode.HTML
                 )
 
-        music_names = re.findall(r"(?<= for \")[^\"]+", logs)
+        if "Skipping" in logs:
+            name = logs.split("Skipping ")[1].split(" (file already exists)")[0]
+        else:
+            name = logs.split('Downloaded "')[1].split('"')[0]
 
-        for name in set(music_names):
-            await message.reply_audio(
-                f"downloads/{name}.mp3",
-                caption=f"<b>{name}</b>\n" f"<code>{spoti_query}</code>",
-                parse_mode=enums.ParseMode.HTML
-            )
-            os.remove(f"downloads/{name}.mp3")
-            await asyncio.sleep(0.5)
+        await message.reply_audio(
+            f"downloads/{name}.mp3",
+            caption=f"<b>{name}</b>\n" f"<code>{spoti_query}</code>",
+        )
+        os.remove(f"downloads/{name}.mp3")
+        await asyncio.sleep(0.5)
         return await message.delete()
     except Exception as e:
-        await message.edit(f"<b>Spotify-Download error:</b>\n{format_exc(e)}", parse_mode=enums.ParseMode.HTML)
+        return await message.edit(
+            f"<b>Spotify-Download error:</b>\n{format_exc(e)}",
+            parse_mode=enums.ParseMode.HTML,
+        )
 
 
 modules_help["spotdl"] = {
