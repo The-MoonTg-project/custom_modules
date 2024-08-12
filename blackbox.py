1c4c9212bf27681f311361b6b9ee474ce9a236ce
NOTHING <70184956+shado-hackers@users.noreply.github.com>
2024-08-12 07:51:33 +0530

    Update blackbox.py

--- blackbox.py
+++ blackbox.py
@@ -6,6 +6,8 @@ from aiohttp import FormData
 from pyrogram import filters, types, enums, errors
 import os
 from utils.misc import modules_help, prefix
+import base64
+import requests
 
 
 session = ClientSession()
@@ -119,8 +121,50 @@ async def blackbox(client, message):
                 text=rdata['reply']
             )
 
+
+
+@Client.on_message(filters.command(["imgur"], prefix) & filters.me)
+async def imgur(client, message):
+    # Check if a reply exists
+    msg = await message.reply_text(
+      "🎉 Please patience. trying to upload..."
+    )
+    if message.reply_to_message and message.reply_to_message.photo:
+        # Download the photo
+        photo_path = await message.reply_to_message.download()
+        # Read the photo file and encode as base64
+        with open(photo_path, "rb") as file:
+            data = file.read()
+            base64_data = base64.b64encode(data)
+        # Set API endpoint and headers for image upload
+        url = "https://api.imgur.com/3/image"
+        headers = {"Authorization": "Client-ID a10ad04550b0648"}
+        # Upload image to Imgur and get URL
+        response = requests.post(url, headers=headers, data={"image": base64_data})
+        result = response.json()
+        await msg.edit_text(result["data"]["link"])
+    elif message.reply_to_message and message.reply_to_message.animation:
+        # Download the animation (GIF)
+        animation_path = await message.reply_to_message.download()
+        # Read the animation file and encode as base64
+        with open(animation_path, "rb") as file:
+            data = file.read()
+            base64_data = base64.b64encode(data)
+        # Set API endpoint and headers for animation upload
+        url = "https://api.imgur.com/3/image"
+        headers = {"Authorization": "Client-ID a10ad04550b0648"}
+        # Upload animation to Imgur and get URL
+        response = requests.post(url, headers=headers, data={"image": base64_data})
+        result = response.json()
+        await msg.edit_text(result["data"]["link"])
+    else:
+        await msg.edit_text("Please reply to a photo or animation (GIF) to upload to Imgur.")
+
+
+
+
 modules_help["sarethai"] = {
     "blackbox [query]*": "Ask anything to Blackbox",
     "bbox [query]*": "Ask anything to Blackbox",
-
+    "imgur [img]*": "upload umg to imgur",
             
