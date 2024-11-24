b15703edf8e4193144103fdd1698c15dcb920337
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-11-25 04:26:42 +0530

    Update apkid.py

--- apkid.py
+++ apkid.py
@@ -1,6 +1,5 @@
 import os
 import time
-import yara
 
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message
@@ -8,8 +7,8 @@ from pyrogram.types import Message
 from utils.misc import modules_help, prefix
 from utils.scripts import progress, import_library
 
-import_library("apkid", "apkid")
-import apkid as ap
+yara = import_library("yara", "yara-python-dex")
+ap = import_library("apkid", "apkid")
 from apkid.apkid import Options, Scanner, OutputFormatter, RulesManager
 
 
@@ -17,7 +16,7 @@ class CustomOutputFormatter(OutputFormatter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def write(self, results): 
+    def write(self, results):
         if self.json:
             return self._build_json_output(results)
         else:
@@ -29,14 +28,14 @@ class CustomOutputFormatter(OutputFormatter):
             match_results = self._build_match_results(raw_matches)
             if len(match_results) == 0:
                 continue
-            output += f"ᴛᴀʀɢᴇᴛ: {key}\n"
+            output += f"ᴛᴀʀɢᴇᴛ: `{key.split(os.sep)[-1]}`\n"
             for tags in sorted(match_results):
-                descriptions = ', '.join(sorted(match_results[tags]))
+                descriptions = ", ".join(sorted(match_results[tags]))
                 if self.include_types:
                     tags_str = self._colorize_tags(tags)
                 else:
                     tags_str = tags
-                output += f" |-> **{tags_str}** : `{descriptions}`\n\n"
+                output += f" |-> **{tags_str}** : `{descriptions}`\n"
         return output
 
 
@@ -48,15 +47,24 @@ async def apkid(_, message: Message):
     if message.reply_to_message:
         text = "Downloading..."
         await message.edit(text)
-        apk = await message.reply_to_message.download(progress=progress, progress_args=(message, time.time(), text))
+        apk = await message.reply_to_message.download(
+            progress=progress, progress_args=(message, time.time(), text)
+        )
         text += "\nFile Downloaded."
         text += "\nScanning File..."
         await message.edit(text)
-        rules_file_path = os.path.join(os.path.dirname(ap.__file__), "rules", "rules.yarc")
+        rules_file_path = os.path.join(
+            os.path.dirname(ap.__file__), "rules", "rules.yarc"
+        )
         rules = yara.load(filepath=rules_file_path)
         options = Options()
         result = Scanner(rules=rules, options=options).scan_file(apk)
-        formatter = CustomOutputFormatter(json_output=False, output_dir=None, rules_manager=RulesManager(), include_types=False)
+        formatter = CustomOutputFormatter(
+            json_output=False,
+            output_dir=None,
+            rules_manager=RulesManager(),
+            include_types=False,
+        )
         output = formatter.write(result)
         if os.path.exists(apk):
             os.remove(apk)
@@ -65,5 +73,6 @@ async def apkid(_, message: Message):
     if os.path.exists(apk):
         os.remove(apk)
     return await message.edit("Please reply to a file!")
+
 
 modules_help["apkid"] = {"apkid": "apkid scanner"}
