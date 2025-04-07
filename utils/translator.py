c3378b71589d95208dd6790a7249f0418f299bc5
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2025-04-07 13:28:42 +0530

    Update translator.py
    Co-authored-by: Kolqvr <kolqvr@users.noreply.github.com>

--- utils/translator.py
+++ utils/translator.py
@@ -1,45 +1,71 @@
-# TODO
-# WARNING! IT COMMENTED FULLY SINCE INSTALL PACKAGES FROM THIS MODULE WILL
-# BROKE ALL FUCKING Moon, NOT JUST THIS MODULE
-# THIS MODULE IS INCOMPATIBLE WITH LAST Moon VERSION
-# SINCE IT USE OUTDATED PACKAGE WITH BROKEN DEPENDENCIES
-# IT NEEDS TO BE REWRITEN
+import json
 
-from utils.scripts import format_small_module_help, import_library
-from utils.misc import modules_help, prefix
+import requests
+
 from pyrogram import Client, filters
 
-googletrans = import_library("googletrans", "googletrans-py")
-from googletrans import Translator
-
-trl = Translator()
+from utils.misc import modules_help, prefix
+from utils.scripts import format_small_module_help
 
 
 @Client.on_message(filters.command(["trans", "tr"], prefix) & filters.me)
 async def translatedl(_client, message):
     try:
+        # Parse command arguments
         if len(message.command) > 1:
-            dtarget = message.text.split(None, 2)[1]
+            dtarget = message.command[1]
         else:
             dtarget = "en"
+
         if len(message.command) > 2:
             dtext = message.text.split(None, 2)[2]
         elif message.reply_to_message:
             dtext = message.reply_to_message.text
         else:
-            message.edit_text(format_small_module_help("translator"))
+            await message.edit_text(format_small_module_help("translator"))
+            return
+
         await message.edit_text("<b>Translating</b>")
-        dtekstr = trl.translate(dtext, dest=dtarget)
+
+        # Use the Google Translate API endpoint
+        url = "https://clients5.google.com/translate_a/t"
+        params = {
+            "client": "dict-chrome-ex",
+            "sl": "auto",
+            "tl": dtarget,
+            "q": dtext
+        }
+
+        response = requests.get(url, params=params)
+
+        if response.status_code != 200:
+            await message.edit_text(f"<b>Error:</b> API returned status code {response.status_code}")
+            return
+
+        # Parse the JSON response
+        data = json.loads(response.text)
+
+        # Based on actual response format [["translated_text", "detected_language"]]
+        if isinstance(data, list) and len(data) > 0 and isinstance(data[0], list) and len(data[0]) > 0:
+            translated_text = data[0][0]
+            # Check if language detection is included
+            source_lang = data[0][1] if len(data[0]) > 1 else "auto"
+        else:
+            translated_text = "Translation failed"
+            source_lang = "unknown"
+
         await message.edit_text(
-            f"<b>Translated</b> to <code>{dtarget}</code> :\n\n"
-            + "{}".format(dtekstr.text)
+            f"<b>Translated</b> from <code>{
+                source_lang}</code> to <code>{dtarget}</code>:\n\n"
+            + "{}".format(translated_text)
         )
-    except ValueError as err:
-        await message.edit("Error: <code>{}</code>".format(str(err)))
+
+    except Exception as err:
+        await message.edit_text(f"<b>Error:</b> <code>{str(err)}</code>")
         return
 
 
 modules_help["translator"] = {
-    "tr": "[lang]* [text/reply]* translate message",
-    "trans": "[lang]* [text/reply]* translate message \n If lang not given it'll use default(en)",
+    "tr": "[lang] [text/reply] translate message",
+    "trans": "[lang] [text/reply] translate message \n\nIf lang not given it'll use default(en)",
 }
