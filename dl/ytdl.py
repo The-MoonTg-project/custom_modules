ff23026075d55f6a584dfafb96412a206c5c0d01
Syed Tahseen <itxtahseen@gmail.com>
2025-04-18 22:46:56 +0500

    Update ytdl.py

--- dl/ytdl.py
+++ dl/ytdl.py
@@ -1,130 +1,134 @@
+import json
+import aiohttp
+import asyncio
 import os
-import time
-import requests
-
 from pyrogram import Client, filters
 from pyrogram.types import Message
-
-from utils.scripts import import_library
-
-yt_dlp = import_library("yt_dlp", "yt-dlp")
-from yt_dlp import YoutubeDL
-
 from utils.misc import modules_help, prefix
-from utils.scripts import format_exc, progress, resize_image
-
-ydv_opts = {
-    "format": "best",
-    "geo_bypass": True,
-    "nocheckcertificate": True,
-    "addmetadata": True,
-    "noplaylist": True,
-    "outtmpl": "videos/%(id)s.%(ext)s",
-}
-
-ydm_opts = {
-    "format": "m4a",
-    "geo_bypass": True,
-    "preferredquality": "320",
-    "nocheckcertificate": True,
-    "addmetadata": True,
-    "noplaylist": True,
-    "outtmpl": "audios/%(id)s.%(ext)s",
-}
 
 
-def download_video(url):
-    with YoutubeDL(ydv_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-        i_d = info.get("id")
-        title = info.get("title")
-        ext = info.get("ext")
-        thumb_url = info.get("thumbnail")
-        print(thumb_url)
-        if thumb_url:
-            resp = requests.get(thumb_url)
-            img_content = resp.content
-            with open("thumbyt.jpg", "wb") as f:
-                f.write(img_content)
-            img = resize_image("thumbyt.jpg")
-        file_path = f"videos/{i_d}.{ext}"
-        ydl.download([url])
-        return file_path, title, img
+async def fetch_youtube_data(query, status_message):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://apis.davidcyriltech.my.id/song?query={query}") as response:
+                result = await response.json()
+
+        if not result.get("status") or not result.get("result"):
+            raise ValueError("No results found")
+
+        return result["result"]
+    except Exception as e:
+        await status_message.edit_text(f"<code>Failed to fetch data: {str(e)}</code>")
+        return None
 
 
-def download_music(url):
-    with YoutubeDL(ydm_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-        i_d = info.get("id")
-        title = info.get("title")
-        thumb_url = info.get("thumbnail")
-        if thumb_url:
-            resp = requests.get(thumb_url)
-            with open("thumbyt.jpg", "wb") as f:
-                f.write(resp.content)
-            img = resize_image("thumbyt.jpg")
-        file_path = f"audios/{i_d}.m4a"
-        ydl.download([url])
-        return file_path, title, img
+async def download_file(url, file_name):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            with open(file_name, "wb") as file:
+                while chunk := await response.content.read(1024):
+                    file.write(chunk)
 
 
-@Client.on_message(filters.command(["ytv", "ytm"], prefix) & filters.me)
-async def upload_video(client: Client, message: Message):
-    if len(message.command) < 2:
-        return await message.reply_text("Please provide a YouTube URL to download.")
+@Client.on_message(filters.command(["yta", "ytaudio"], prefix) & filters.me)
+async def youtube_audio_download(client: Client, message: Message):
+    is_self = message.from_user and message.from_user.is_self
+    query = (
+        message.text.split(maxsplit=1)[1]
+        if len(message.command) > 1
+        else getattr(message.reply_to_message, "text", "").strip()
+    )
 
-    if message.command[0] == "ytv":
-        await message.edit_text("Starting Download...")
-        try:
-            url = message.text.split(maxsplit=1)[1]
-            # await message.edit_text("Starting Download...")
-            file_path, title, img = download_video(url)
+    if not query:
+        response = f"<b>Usage:</b> <code>{prefix}ytdla [query]</code>"
+        await (message.edit(response) if is_self else message.reply(response))
+        return
 
-            ms = await message.edit_text("Uploading Video...")
-            c = time.time()
+    status_message = await (
+        message.edit_text(f"<code>Searching for {query} on YouTube...</code>")
+        if is_self
+        else message.reply(f"<code>Searching for {query} on YouTube...</code>")
+    )
 
-            await client.send_video(
-                message.chat.id,
-                video=file_path,
-                thumb=img,
-                caption=f"<code>{title}<code/>",
-                progress=progress,
-                progress_args=(ms, c, "`Uploading Video...`"),
-            )
-            os.remove(file_path)
-            os.remove("thumbyt.jpg")
-            await message.delete()
-        except Exception as e:
-            await message.edit_text(f"An error occurred: {format_exc(e)}")
-    elif message.command[0] == "ytm":
-        await message.edit_text("Starting Download...")
-        try:
-            url = message.text.split(None, 1)[1]
-            file_path, title, img = download_music(url)
+    youtube_data = await fetch_youtube_data(query, status_message)
+    if not youtube_data:
+        return
 
-            os.rename(file_path, f"audios/{title}.m4a")
+    title = youtube_data["title"]
+    audio_url = youtube_data["audio"]["download_url"]
+    audio_format = youtube_data["audio"]["format"]
+    audio_quality = youtube_data["audio"]["quality"]
 
-            ms = await message.edit_text("Uploading Audio...")
-            c = time.time()
+    await status_message.edit_text(f"<code>Downloading audio: {title}...</code>")
+    file_name = f"{title}.{audio_format.lower()}"
 
-            await client.send_audio(
-                message.chat.id,
-                audio=f"audios/{title}.m4a",
-                thumb=img,
-                caption=f"<code>{title}<code/>",
-                progress=progress,
-                progress_args=(ms, c, "`Uploading Audio...`"),
-            )
-            os.remove(f"audios/{title}.m4a")
-            os.remove("thumbyt.jpg")
-            await message.delete()
-        except Exception as e:
-            await message.edit_text(f"An error occurred: {format_exc(e)}")
-    else:
-        return await message.edit_text("Oh Damn Lol")
+    try:
+        await download_file(audio_url, file_name)
+
+        await client.send_audio(
+            message.chat.id,
+            file_name,
+            caption=f"<b>Title:</b> {title}\n<b>Format:</b> {audio_format}\n<b>Quality:</b> {audio_quality}",
+        )
+    except Exception as e:
+        await status_message.edit_text(f"<code>Failed to download audio: {str(e)}</code>")
+    finally:
+        if os.path.exists(file_name):
+            os.remove(file_name)
+
+    await status_message.delete()
 
 
-modules_help["ytdl"] = {
-    "ytv [link]*": "Download Video From YouTube",
-    "ytm [link]*": "Download Music From YouTube",
+@Client.on_message(filters.command(["ytv", "ytvideo"], prefix) & filters.me)
+async def youtube_video_download(client: Client, message: Message):
+    is_self = message.from_user and message.from_user.is_self
+    query = (
+        message.text.split(maxsplit=1)[1]
+        if len(message.command) > 1
+        else getattr(message.reply_to_message, "text", "").strip()
+    )
+
+    if not query:
+        response = f"<b>Usage:</b> <code>{prefix}ytdlv [query]</code>"
+        await (message.edit(response) if is_self else message.reply(response))
+        return
+
+    status_message = await (
+        message.edit_text(f"<code>Searching for {query} on YouTube...</code>")
+        if is_self
+        else message.reply(f"<code>Searching for {query} on YouTube...</code>")
+    )
+
+    youtube_data = await fetch_youtube_data(query, status_message)
+    if not youtube_data:
+        return
+
+    title = youtube_data["title"]
+    video_url = youtube_data["video"]["download_url"]
+    video_format = youtube_data["video"]["format"]
+    video_quality = youtube_data["video"]["quality"]
+
+    await status_message.edit_text(f"<code>Downloading video: {title}...</code>")
+    file_name = f"{title}.{video_format.lower()}"
+
+    try:
+        await download_file(video_url, file_name)
+
+        await client.send_video(
+            message.chat.id,
+            file_name,
+            caption=f"<b>Title:</b> {title}\n<b>Format:</b> {video_format}\n<b>Quality:</b> {video_quality}",
+        )
+    except Exception as e:
+        await status_message.edit_text(f"<code>Failed to download video: {str(e)}</code>")
+    finally:
+        if os.path.exists(file_name):
+            os.remove(file_name)
+
+    await status_message.delete()
+
+
+modules_help["youtube"] = {
+    "yta [query]": "search and download audio from YouTube",
+    "ytv [query]": "search and download video from YouTube",
 }
