cd921da709e8efd0ea1eab867c4476c862a892af
Doctor Code <87318660+DrCode32@users.noreply.github.com>
2025-07-31 00:45:06 +0330

    Update raw.py

--- utils/raw.py
+++ utils/raw.py
@@ -17,17 +17,17 @@ async def get_raw_message(client: Client, message: Message):
     chunk_size = 4096 - 10
     
     if len(json_data) <= chunk_size:
-        await client.send_message("me", f"```json\n{json_data}\n```", parse_mode="Markdown")
+        await client.send_message("me", f"```json\n{json_data}\n```", parse_mode="markdown")
     else:
         chunks = [json_data[i:i+chunk_size] for i in range(0, len(json_data), chunk_size)]
         
         for i, chunk in enumerate(chunks):
             if i == 0:
-                await client.send_message("me", f"```json\n{chunk}", parse_mode="Markdown")
+                await client.send_message("me", f"```json\n{chunk}", parse_mode="markdown")
             elif i == len(chunks) - 1:
-                await client.send_message("me", f"{chunk}\n```", parse_mode="Markdown")
+                await client.send_message("me", f"{chunk}\n```", parse_mode="markdown")
             else:
-                await client.send_message("me", chunk, parse_mode="Markdown")
+                await client.send_message("me", chunk, parse_mode="markdown")
     
     await message.delete()
 
