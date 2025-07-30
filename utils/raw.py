3d3997b46b5e376a2f4444385f55e0d50833f021
Doctor Code <87318660+DrCode32@users.noreply.github.com>
2025-07-31 01:02:32 +0330

    Update raw.py

--- utils/raw.py
+++ utils/raw.py
@@ -11,22 +11,17 @@ async def get_raw_message(client: Client, message: Message):
         return
     
     raw_data = str(message.reply_to_message)
-    # فرمت‌بندی JSON
     json_data = json.dumps(json.loads(raw_data), indent=2, ensure_ascii=False)
     
-    # چون به هر تکه '```json\n' و '\n```' اضافه می‌کنیم، فضای بیشتری لازم داریم
-    # 4096 (حد تلگرام) - 15 (کاراکترهای اضافه شده)
     chunk_size = 4080 
     
     if len(json_data) <= chunk_size:
-        # برای پیام‌های کوتاه، یک بلاک کد ارسال می‌شود
         await client.send_message(
             "me",
             f"```json\n{json_data}\n```",
             parse_mode=enums.ParseMode.MARKDOWN
         )
     else:
-        # برای پیام‌های طولانی، هر تکه در یک بلاک کد جداگانه ارسال می‌شود
         chunks = [json_data[i:i+chunk_size] for i in range(0, len(json_data), chunk_size)]
         
         for chunk in chunks:
