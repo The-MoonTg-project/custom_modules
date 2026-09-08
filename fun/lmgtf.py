e261984997ce8879f1d9cc62f4efc597f23dc2df
ramanveerji <ramandeepmatharu@gmail.com>
2026-09-08 12:52:07 +0000

    feat(lmgtf): add caption support when replying to media messages

--- fun/lmgtf.py
+++ fun/lmgtf.py
@@ -10,8 +10,8 @@ async def lmgtf_cmd(client: Client, message: Message):
     query = ""
     if len(message.command) > 1:
         query = " ".join(message.command[1:])
-    elif message.reply_to_message and message.reply_to_message.text:
-        query = message.reply_to_message.text
+    elif message.reply_to_message and (message.reply_to_message.text or message.reply_to_message.caption):
+        query = message.reply_to_message.text or message.reply_to_message.caption
 
     if not query:
         await message.edit(
